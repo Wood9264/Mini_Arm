0526b66a1edfa467aa559171d0a1bdc38736576b
Wood <1831051389@qq.com>
2025-06-09 16:48:43 +0800

    perf: 因步进电机引脚占用了led引脚，停止led任务的运行

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,8 @@ void setup()
     Serial.begin(115200);
 
     xTaskCreatePinnedToCore(armTaskInstance.runTask, "ARM Task", 2048, NULL, 1, &armTaskHandle, 1);
-    xTaskCreatePinnedToCore(ledTaskInstance.runTask, "LED Task", 1024, NULL, 1, &ledTaskHandle, 1);
+    // 因步进电机引脚占用了led引脚，停止led任务的运行
+    // xTaskCreatePinnedToCore(ledTaskInstance.runTask, "LED Task", 1024, NULL, 1, &ledTaskHandle, 1);
     xTaskCreatePinnedToCore(joystickTaskInstance.runTask, "Joystick Task", 4096, NULL, 1, &joystickTaskHandle, 1);
 }
 

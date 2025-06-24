3917b1565c317bb2c58edff04b34368795c8c509
Wood <1831051389@qq.com>
2025-06-24 22:59:48 +0800

    style: 统一任务名称的大小写格式

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,9 @@ void setup()
 {
     Serial.begin(115200);
 
-    xTaskCreatePinnedToCore(armTaskInstance.runTask, "ARM Task", 2048, NULL, 1, &armTaskHandle, 1);
+    xTaskCreatePinnedToCore(armTaskInstance.runTask, "Arm Task", 2048, NULL, 1, &armTaskHandle, 1);
     // 因步进电机引脚占用了led引脚，停止led任务的运行
-    // xTaskCreatePinnedToCore(ledTaskInstance.runTask, "LED Task", 1024, NULL, 1, &ledTaskHandle, 1);
+    // xTaskCreatePinnedToCore(ledTaskInstance.runTask, "Led Task", 1024, NULL, 1, &ledTaskHandle, 1);
     xTaskCreatePinnedToCore(joystickTaskInstance.runTask, "Joystick Task", 4096, NULL, 1, &joystickTaskHandle, 1);
 }
 
